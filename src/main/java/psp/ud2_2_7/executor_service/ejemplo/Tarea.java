7f3d83559323cbce44219c3c4e3c945a219c4c3f
Ruben Leis <rubenleisfp@gmail.com>
2025-09-15 13:15:35 +0200

    Limpiar para estudiantes

--- src/main/java/psp/ud2_2_7/executor_service/ejemplo/Tarea.java
+++
@@ -1,8 +0,0 @@
-package psp.ud2_2_7.executor_service.ejemplo;
-
-class Tarea implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Ejecutando la tarea en el hilo: " + Thread.currentThread().getName());
-    }
-}
